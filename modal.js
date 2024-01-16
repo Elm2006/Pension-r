8f7cbcf9484736e519caa13a29add11d7c08bd42
Elm2006 <143172014+Elm2006@users.noreply.github.com>
2024-01-16 22:35:50 +0100

    popup

---
+++ modal.js
@@ -0,0 +1,41 @@
+const popup = document.querySelector('.popup');
+const openPopup = document.querySelector('.openPopup')
+const closePopup = document.querySelectorAll('.closePopup');
+const popupDescription = document.querySelector('.popupDescription');
+const popupTitle =  document.querySelector('.popupTitle');
+openPopup.addEventListener('click', () => {
+    popup.showModal();
+});
+closePopup.addEventListener('click', () => {
+    popup.close();
+});
+const eventsText={
+    1:[
+        "Cykla runt växjösjön"
+    ],
+    2:[
+        "Bingo med Berra"
+    ],
+ };
+const eventsTitle={
+1:[
+    "Cykling"
+],
+2:[
+    "Bingo"
+],
+};
+function event2(){
+    n=2;
+    const title = eventsTitle[n];
+    const text=eventsText[n];
+    popupTitle.textContent=title;
+    popupDescription.textContent=text;
+};
+function event1(){
+    n=1;
+    const title = eventsTitle[n];
+    const text=eventsText[n];
+    popupTitle.textContent=title;
+    popupDescription.textContent=text;
+};
