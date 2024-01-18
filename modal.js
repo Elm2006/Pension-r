ab1f882a784ab240292606cb119fb22a01f335a0
sere06001 <sere06001@utb.vaxjo.se>
2024-01-18 19:13:30 +0100

    aaa

--- modal.js
+++ modal.js
@@ -2,7 +2,7 @@ const popup = document.querySelector('.popup');
 const openPopup = document.querySelector('.openPopup')
 const closePopup = document.querySelector('.closePopup');
 const popupDescription = document.querySelector('.popupDescription');
-const popupTitle =  document.querySelector('.popupTitle');
+const popupTitle = document.querySelector('.popupTitle');
 openPopup.addEventListener('click', () => {
     popup.showModal();
 });
@@ -10,12 +10,21 @@ closePopup.addEventListener('click', () => {
     popup.close();
 });
 const eventsText={
-    1:["Cykla runt växjösjön"],
+    1:["Promenad runt växjosjön"],
     2:["Bingo med Berra"],
+    3:["Kort beskrivning"],
  };
 const eventsTitle={
-1:["Cykling"],
+1:["Promenad"],
 2:["Bingo"],
+3:["Omfattande titel"],
+};
+function event1(){
+    n=1;
+    const title = eventsTitle[n];
+    const text=eventsText[n];
+    popupTitle.textContent=title;
+    popupDescription.textContent=text;
 };
 function event2(){
     n=2;
@@ -24,8 +33,8 @@ function event2(){
     popupTitle.textContent=title;
     popupDescription.textContent=text;
 };
-function event1(){
-    n=1;
+function event3(){
+    n=3;
     const title = eventsTitle[n];
     const text=eventsText[n];
     popupTitle.textContent=title;
